079305dfc966ee56583c11f71cd3f589ceb836a3
paulafranceschini <paula.franceschini@gmail.com>
2020-11-27 19:25:32 -0300

    atualizacoes

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
 
 app.listen(process.env.PORT || 8080);
