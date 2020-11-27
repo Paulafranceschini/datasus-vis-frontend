845fbe88e9e881898596a58b25c2576f7e6aec79
paulafranceschini <paula.franceschini@gmail.com>
2020-11-27 18:48:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static(`${__dirname}/`));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/index.html`));
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 app.listen(process.env.PORT || 8080);
