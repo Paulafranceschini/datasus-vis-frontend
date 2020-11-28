a3588215db26350a1ebaf377e8be706350c11ec0
paulafranceschini <paula.franceschini@gmail.com>
2020-11-28 16:01:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const nomeApp = process.env.npm_package_name;
 const app = express();
 
 app.use(express.static(`dist`));
-
+console.log("dirname = " + __dirname)
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`dist/index.html`));
+res.sendFile(path.join(__dirname +`/dist/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
